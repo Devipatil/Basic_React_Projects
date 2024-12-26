b256548636efce88815d2657462c35773ea588f4
Devi Patil <devipatil2001@gmail.com>
2024-12-26 12:50:53 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,18 @@
-
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Greeting from "./Greeting";
 function App() {
+  let name = "Pooja";
   return (
+    <div>
     <h1>Hello</h1>
+    <Greeting name = {'Akash'} />
+    <Greeting name = {'Ayush'} />
+    {/* <Home name = {name} age = {24} />
+    <About />
+    <Contact /> */}
+    </div>
   );
 }
 
